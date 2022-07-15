c9bab75de4aba5d467a4fe01145a01c3eaf060b6
[Jeremy] <[salinasjeremy2@gmail.com]>
2022-07-15 01:18:59 -0500

    added score system

--- index.js
+++ index.js
@@ -25,33 +25,56 @@ function computerPlay(userChoice){
         }else if(botChoice === 3){
             return "draw"
         }
+
+
+        if(botChoice === 1){
+            document.querySelector(".generated").style.background= 'url(img/rock.png)'
+        }else if(botChoice === 2){
+            document.querySelector(".generated").style.background= 'url(img/paper.png)'
+        }else if(botChoice === 3){
+            document.querySelector(".generated").style.background= 'url(img/scissors.png)'
+        }
     }
 }
 
-function playgame(){
-    let outcome = document.createElement('p')
-    outcome.style.color = 'black'
-    outcome.style.fontFamily = 'tom hand'
-    outcome.style.fontSize = "45px"
-    outcome.style.textAlign = 'center'
+    let userScore=0
+    let computScore=0
+    let outcome = document.createElement('div')
+    let userScoreText = document.createElement('div')
+    let computScoreText = document.createElement('div')
 
         document.getElementById("choicesUI").addEventListener('click', e =>{
-            for(let i=0; i<3;i++){
-            if(e.target.innerText === "Rock"){
-                outcome.textContent = computerPlay("rock")
+
+            if(userScore > 4 || computScore > 4){
+                if(userScore === 5){
+                    outcome.textContent = "You win"
+                }else if(computScore === 5){
+                    outcome.textContent = "You lose"
+                }
+                document.createElement
+                gameOutcome = "draw"
+            }else if(e.target.innerText === "Rock"){
+                gameOutcome = computerPlay("rock")
+                outcome.textContent = gameOutcome
             }else if(e.target.innerText === "Paper"){
-                outcome.textContent = computerPlay("paper")
+                gameOutcome = computerPlay("paper")
+                outcome.textContent = gameOutcome
             }else if (e.target.innerText === "Scissors"){
-                outcome.textContent = computerPlay("scissors")
-            }
-            console.log(i)
+                gameOutcome = computerPlay("scissors")
+                outcome.textContent = gameOutcome
             }
 
+            if(gameOutcome === "win"){
+                userScore ++
+            }else if(gameOutcome ==="lose"){
+                computScore ++
+            }
+            console.log(userScoreText)
+            userScoreText.textContent = userScore
+            computScoreText.textContent = computScore
+
+        document.getElementById('outcome').appendChild(userScoreText);
         document.getElementById('outcome').appendChild(outcome);
+        document.getElementById('outcome').appendChild(computScoreText);
+
         })
-
-}
-
-for(let i=0; i<3; i++){
-    playgame()
-}
